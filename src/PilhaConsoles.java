d790e3920cb62a00d4a52e8bcacac8eae5fd6d34
warlley <euwarlley@dominiolechuga.com>
2026-05-20 01:03:41 -0300

    ajeitei as variaveis necessarias e criei o metodo de push juntamente com o verificador de limite de tamanho para duplicar

--- src/PilhaConsoles.java
+++ src/PilhaConsoles.java
@@ -1,5 +1,25 @@
 public class PilhaConsoles {
 
     private Console[] pilhaDeConsoles = new Console[5];
+    private int topoDosConsoles = 0;
+
+    public void pilhaPush(Console newConsole){
+        pilhaDeConsoles[topoDosConsoles] = newConsole;
+        topoDosConsoles++;
+
+        if (pilhaDeConsoles.length == topoDosConsoles){
+            Console[] novaPilhaDeConsoles = new Console [pilhaDeConsoles.length * 2];
+
+            for (int i = 0; i < pilhaDeConsoles.length; i++){
+                novaPilhaDeConsoles[i] = pilhaDeConsoles [i];
+            }
+
+            pilhaDeConsoles = novaPilhaDeConsoles;
+        }
+    }
+
+    public void pilhaPop(){
+
+    }
 
 }
